6ba0ae872f6f6c61c6b6e01f7233d1c9937b702d
syeutyu <syeuty@naver.com>
2017-11-29 10:56:12 +0900

    [UPDATE] remove files solution

--- File/main.py
+++ File/main.py
@@ -1,5 +1,6 @@
 from flask import Flask,request,render_template
 from werkzeug  import secure_filename
+import os
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,7 +12,8 @@ def save():
     print(request.method) # get 방식으로는 접근이 불가
     f = request.files.get('file')
     f.save('saveFolder/'+f.filename) # 폴더명을 명시하지않을경우 main 폴더 부분에 사진이 생성됩니다
-    print(f.filename)
+
+    os.remove('saveFolder/'+f.filename) # 파일 삭제
     return 'success'
 
 if __name__ == '__main__':
