d48c001a3b7a59ae055e058f3f4572b478e96b9b
syeutyu <syeuty@naver.com>
2017-12-03 23:41:56 +0900

    read json file

--- Json/makeJson.py
+++ Json/makeJson.py
@@ -6,5 +6,24 @@ obj["name"] = "이동현"
 obj["age"] = '9'
 obj["address"] = "청주"
 
-print(obj)
 print(json.dumps(obj, ensure_ascii=False, indent="\t"))
+
+"""
+응답 json
+{
+	"name": "이동현",
+	"age": "9",
+	"address": "청주"
+}
+"""
+
+with open('test.json','r') as make:
+    data = json.load(make)
+    print(data)
+
+"""
+{
+    "add":123,
+    "test":"hello"
+}
+"""
