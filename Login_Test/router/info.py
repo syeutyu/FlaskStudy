0166a6bfe87a21b9652e5cb912fa4cbf9bbfd80d
syeutyu <syeuty@naver.com>
2017-12-20 11:40:41 +0900

    [UPDATE] JWT SignIn Response

--- Login_Test/router/info.py
+++ Login_Test/router/info.py
@@ -1,4 +1,3 @@
-from flask_jwt_extended import *
 from flask_restful import request,Resource
 from Login_Test.Mongo.schema import  User
 from flask_jwt_extended import *
@@ -14,13 +13,18 @@ class SignUp(Resource):
         user_id = request.form.get('user_id')
         password = request.form.get('password')
         User(user_id=user_id,password=password).save()
-        response = jsonify(create= create_access_token(identity=password),respresh=create_refresh_token(identity=password))
-        response.status_code = 201
-        return response
+        return Response('',201)
 
 class Singin(Resource):
-    @jwt_required
     def post(self):
-       user_token = get_jwt_identity()
-       return jsonify(check_token=user_token)
+        user_id = request.form.get('user_id')
+        password = request.form.get('password')
+        find = User.objects(user_id=user_id,password=password)
+        if(find):
+            response = jsonify(create=create_access_token(identity=password),
+                               respresh=create_refresh_token(identity=password))
+            response.status_code = 201
+            return response
+        else :
+            return Response('',400)
 
