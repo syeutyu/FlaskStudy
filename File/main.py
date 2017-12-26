f444ac78baf810a57b55f50251511c78f714b68d
syeutyu <syeuty@naver.com>
2017-12-26 22:32:46 +0900

    [UPDATE] File Path Select

--- File/main.py
+++ File/main.py
@@ -11,9 +11,8 @@ def render():
 def save():
     print(request.method) # get 방식으로는 접근이 불가
     f = request.files.get('file')
-    f.save('saveFolder/'+f.filename) # 폴더명을 명시하지않을경우 main 폴더 부분에 사진이 생성됩니다
-
-    os.remove('saveFolder/'+f.filename) # 파일 삭제
+    f.save(os.path.dirname(os.path.abspath(__file__))+'\\saveFolder\\'+f.filename) # 폴더명을 명시하지않을경우 main 폴더 부분에 사진이 생성됩니다
+    os.remove(os.path.dirname(os.path.abspath(__file__))+'\\saveFolder\\'+f.filename) # 파일 삭제
     return 'success'
 
 if __name__ == '__main__':
