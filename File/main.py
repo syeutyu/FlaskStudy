f0e88a1d8c00263ab60c95a9e5ad1549919f7196
syeutyu <syeuty@naver.com>
2017-12-28 10:51:38 +0900

    [DONE] File Path Final

--- File/main.py
+++ File/main.py
@@ -11,8 +11,9 @@ def render():
 def save():
     print(request.method) # get 방식으로는 접근이 불가
     f = request.files.get('file')
-    f.save(os.path.dirname(os.path.abspath(__file__))+'\\saveFolder\\'+f.filename) # 폴더명을 명시하지않을경우 main 폴더 부분에 사진이 생성됩니다
-    os.remove(os.path.dirname(os.path.abspath(__file__))+'\\saveFolder\\'+f.filename) # 파일 삭제
+    f.save(os.path.dirname(os.path.abspath(__file__))+'\\saveFolder\\save\\' +f.filename) # os path 는 서버의 실행 위치를 지정합니다
+    # 폴더명을 명시하지않을경우 main 폴더 부분에 사진이 생성됩니다
+    #os.remove(os.path.dirname(os.path.abspath(__file__))+'\saveFolder\save'+f.filename) # 파일 삭제
     return 'success'
 
 if __name__ == '__main__':
